4ee762304a7894a9fff76d6768aeba9e899db2ab
aman-d-1-n-only <abhitheone456@gmail.com>
2023-08-10 02:01:49 +0530

    setting up the in-memory db

--- bank-api/Program.cs
+++ bank-api/Program.cs
@@ -1,3 +1,7 @@
+using BankApi.Models;
+using BankApi.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AdminContext>(options => options.UseInMemoryDatabase("BankDb"));
 
 var app = builder.Build();
 
