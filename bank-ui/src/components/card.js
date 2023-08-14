4c5ba32b5294eee7d978e3f5881643e5cdfde69d
Abhirambvss <abhiram.bsvs@gmail.com>
2023-08-14 11:09:54 +0530

    dffdf

--- bank-ui/src/components/card.js
+++ bank-ui/src/components/card.js
@@ -1,19 +1,36 @@
 import React from "react";
 import { useNavigate } from 'react-router-dom';
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Typography,
+    Button,
+} from "@material-tailwind/react";
+
 
 export const CustomerCard = (props) => {
     const navigate = useNavigate()
     return (
-        <div className="grid m-2 h-15 w-15 border-4 border-black-500 p-2">
 
 
-            <button className="mb-2"
-                onClick={() => navigate('customerDetails')}>
-                {props.cardName}
-            </button>
-
-
-
-        </div>
+        <Card className="mt-6 w-96 p-2 bg-white rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl">
+            <CardHeader color="blue-gray" className="relative h-56">
+                <img src={props.img}
+                    alt="card-image"
+                />
+            </CardHeader>
+            {/* <CardBody>
+                  <Typography variant="h5" color="blue-gray" className="mb-2">
+                   Add Customer Details
+                  </Typography>
+                  
+                </CardBody> */}
+            <CardFooter className="pt-0 mt-6">
+                <Button onClick={() => navigate('customerDetails')}>
+                    {props.cardName}</Button>
+            </CardFooter>
+        </Card>
     );
 }
